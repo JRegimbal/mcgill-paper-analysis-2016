bab235458539941113525dae9eb07154791702ad
JRegimbal <juliette.regimbal@gmail.com>
2017-11-15 14:47:54 -0500

    add word counting for tribune python script
    more accurate than daily simple because the whole HTML formatting here
    ACTUALLY MAKES SENSE. also no exceptions and everything follows the same
    page format, which keeps things nice and simple.

--- categories-tribune.py
+++ categories-tribune.py
@@ -1,4 +1,5 @@
 import re
+import operator
 from subprocess import call
 
 files = open('article-list.txt', 'w')
@@ -10,7 +11,7 @@ call(
 files.close()
 
 files = open('article-list.txt', 'r')
-categories = dict({"uncategorized":0})
+categories = dict({"uncategorized":(0,0)})
 for line in files:
     if line[len(line)-1] is '\n':
         line = line[:-1]
@@ -19,16 +20,34 @@ for line in files:
     article.close()
 
     m = re.search('article:published.+2016.+>', content)
-    if m is not None:
+    if m is not None:   # article was published in 2016
         category = re.search('[a-z-]+', line).group()
+        # try to establish work count
+        entry = re.search('<div class="entry-content">(.|\n)+?<\/div>', content).group()
+        articleList = re.split('<.+?>|&[a-zA-Z]+?;', entry)
+        text = str()
+        for s in articleList:
+            if s is not '':
+                text += s
+        words = 0
+        for s in re.split(' |\n', text):
+            if s is not '':
+                words += 1
         if category in categories:
-            categories[category] += 1
+            categories[category] = tuple(map(
+                operator.add, categories[category], (1, words)))
         else:
-            categories[category] = 1
+            categories[category] = (1, words)
+
 files.close()
 
 total = 0
 for key in categories:
-    total += categories[key]
-    print(str(key)+": "+str(categories[key]))
+    total += categories[key][0]
+    if categories[key][0] is 0:
+        avgwords = 0
+    else:
+        avgwords = int(categories[key][1]/categories[key][0])
+    line = [str(key), str(categories[key][0]), str(avgwords)]
+    print('{:>16} {:>8} {:>8}'.format(*line))
 print("\nTotal: "+str(total))
